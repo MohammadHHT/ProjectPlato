ba4ff5bca5a59aa4ddb37a5bdad85c54a5466a7f
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 18:52:25 +0330

    add User Class fields.

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,14 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class User {
+    private String firstname;
+    private String lastname;
+    private String username;
+    private long userID;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private static ArrayList<User> users;
+}
