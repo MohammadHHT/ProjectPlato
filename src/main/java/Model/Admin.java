f3a4cd8a1dc919042b3990589aef525827e589f1
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 19:06:47 +0330

    set Admin child of User and add related constructor.

--- src/main/java/Model/Admin.java
+++ src/main/java/Model/Admin.java
@@ -1,4 +1,8 @@
 package Model;
 
-public class Admin {
+public class Admin extends User {
+
+    public Admin(String firstname, String lastname, String username, long userID, String password, String email, String phoneNumber) {
+        super(firstname, lastname, username, userID, password, email, phoneNumber);
+    }
 }
