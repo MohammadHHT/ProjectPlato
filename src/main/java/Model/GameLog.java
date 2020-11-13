9a6a44c30ffaf825a5e594e6b064ee9d05f73d8b
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 20:34:37 +0330

    add Setter & Getter methods for GameLog Class.

--- src/main/java/Model/GameLog.java
+++ src/main/java/Model/GameLog.java
@@ -8,4 +8,46 @@ public class GameLog {
     private long logID;
     private long takenScore;
     private static ArrayList<GameLog> gameLogs;
+
+    //TODO Constructor
+
+    public void setNumberOfTimesPlayed(int numberOfTimesPlayed) {
+        this.numberOfTimesPlayed = numberOfTimesPlayed;
+    }
+
+    public void setNumberOfWins(int numberOfWins) {
+        this.numberOfWins = numberOfWins;
+    }
+
+    public void setLogID(long logID) {
+        this.logID = logID;
+    }
+
+    public void setTakenScore(long takenScore) {
+        this.takenScore = takenScore;
+    }
+
+    public static void setGameLogs(ArrayList<GameLog> gameLogs) {
+        GameLog.gameLogs = gameLogs;
+    }
+
+    public int getNumberOfWins() {
+        return numberOfWins;
+    }
+
+    public int getNumberOfTimesPlayed() {
+        return numberOfTimesPlayed;
+    }
+
+    public static ArrayList<GameLog> getGameLogs() {
+        return gameLogs;
+    }
+
+    public long getLogID() {
+        return logID;
+    }
+
+    public long getTakenScore() {
+        return takenScore;
+    }
 }
