f793621e2c19d8f55fe1244598161f0f352e6133
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 18:56:55 +0330

    add User Class Constructor.

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -11,4 +11,18 @@ public class User {
     private String email;
     private String phoneNumber;
     private static ArrayList<User> users;
+
+    static {
+        users = new ArrayList<User>();
+    }
+
+    public User(String firstname, String lastname, String username, long userID, String password, String email, String phoneNumber) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.userID = userID;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
 }
