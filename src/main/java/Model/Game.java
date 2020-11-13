339ecc34a55b6811f7975a9d8afbb38ed831a0a9
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 19:15:44 +0330

    add Game Class fields.

--- src/main/java/Model/Game.java
+++ src/main/java/Model/Game.java
@@ -1,4 +1,14 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Game {
+    private String gameName;
+    private String gameID;
+    private static ArrayList<Game> games;
+
+    static {
+        games = new ArrayList<Game>();
+    }
+
 }
