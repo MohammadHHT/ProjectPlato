b4a0375847310f723815491d61091b6b6f90b43f
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 19:55:36 +0330

    add Event Class fields.

--- src/main/java/Model/Event.java
+++ src/main/java/Model/Event.java
@@ -1,4 +1,15 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Event {
+    private Game gameName;
+    private Date startDate;
+    private Date endDate;
+    private long eventScore;
+    private String eventID;
+    private static ArrayList<Event> events;
+
+
 }
