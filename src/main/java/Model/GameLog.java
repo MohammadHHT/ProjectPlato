87f2ac5d820b3655e23031b4d7c631e63a17da9e
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 19:54:34 +0330

    add GameLog Class fields.

--- src/main/java/Model/GameLog.java
+++ src/main/java/Model/GameLog.java
@@ -1,4 +1,11 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class GameLog {
+    private int numberOfTimesPlayed;
+    private int numberOfWins;
+    private long logID;
+    private long takenScore;
+    private static ArrayList<GameLog> gameLogs;
 }
