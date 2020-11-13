7fdb47b78d4a129afecb4717047c77f472042193
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 19:11:03 +0330

    add Player Class fields.

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,4 +1,15 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Player {
+    private Date platoAge;
+    private double money;
+    private long score;
+    private static ArrayList<Player> players;
+    private ArrayList<Player> friends;
+    private ArrayList<Player> friendRequest;
+    //TODO gamesLog
+
 }
