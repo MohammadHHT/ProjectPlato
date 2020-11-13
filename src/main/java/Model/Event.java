33bacd129c65fc77554f9a439428ef3aa2eb3a26
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 19:56:49 +0330

    add Event Class constructor.

--- src/main/java/Model/Event.java
+++ src/main/java/Model/Event.java
@@ -8,8 +8,18 @@ public class Event {
     private Date startDate;
     private Date endDate;
     private long eventScore;
-    private String eventID;
+    private long eventID;
     private static ArrayList<Event> events;
 
+    static {
+        events = new ArrayList<Event>();
+    }
 
+    public Event(Game gameName, Date startDate, Date endDate, long eventScore, long eventID) {
+        this.gameName = gameName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.eventScore = eventScore;
+        this.eventID = eventID;
+    }
 }
