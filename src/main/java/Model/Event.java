2cc82187583e1288e946a7cc26c55945bb8c43fd
aminlotfi <a.lotfi1201@gmail.com>
2020-11-13 20:42:15 +0330

    add Setter & Getter methods for Event Class.

--- src/main/java/Model/Event.java
+++ src/main/java/Model/Event.java
@@ -22,4 +22,60 @@ public class Event {
         this.eventScore = eventScore;
         this.eventID = eventID;
     }
+
+    public void addNewEvent(){
+        //TODO
+    }
+
+    public void deleteEvent(){
+        //TODO
+    }
+
+    public void setGameName(Game gameName) {
+        this.gameName = gameName;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setEventScore(long eventScore) {
+        this.eventScore = eventScore;
+    }
+
+    public void setEventID(long eventID) {
+        this.eventID = eventID;
+    }
+
+    public static void setEvents(ArrayList<Event> events) {
+        Event.events = events;
+    }
+
+    public Game getGameName() {
+        return gameName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public long getEventScore() {
+        return eventScore;
+    }
+
+    public long getEventID() {
+        return eventID;
+    }
+
+    public static ArrayList<Event> getEvents() {
+        return events;
+    }
 }
