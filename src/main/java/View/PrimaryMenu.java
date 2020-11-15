7ab4e78deff84012bbe4bd7bb566002d9c5a1cc3
mahdi hadi <mahdihadiam@gmail.com>
2020-11-15 12:54:30 +0330

    add Primary Menu class

---
+++ src/main/java/View/PrimaryMenu.java
@@ -0,0 +1,31 @@
+package View;
+
+import Model.User;
+
+public class PrimaryMenu extends Menu {
+    private static PrimaryMenu primaryMenu;
+
+    private PrimaryMenu() {
+    }
+
+    public static PrimaryMenu getInstance() {
+        if (primaryMenu == null) {
+            return new PrimaryMenu();
+        }
+        return primaryMenu;
+    }
+
+    public void showPoint(User user) {}
+
+    public void showFavoriteGames(User user) {}
+
+    public void showBotMessages(User user) {}
+
+    public void showBotSuggestions(User user) {}
+
+    public void playSuggested(User user) {}
+
+    public void showLastGame(User user) {}
+
+    public boolean addFriend(User user, String friend) { return true; }
+}
