4db00e50c92fd1d47654fac7a0b763de479018a9
aminlotfi <a.lotfi1201@gmail.com>
2020-11-16 18:31:19 +0330

    handle jdk thing :/

--- src/main/java/View/Menu.java
+++ src/main/java/View/Menu.java
@@ -4,7 +4,7 @@ import Model.User;
 import java.util.ArrayList;
 
 abstract class Menu {
-    private static ArrayList<Menu> tree = new ArrayList<>();
+    private static ArrayList<Menu> tree = new ArrayList<Menu>();
     private static User user;
 
     public static User getUser() { return user; }
