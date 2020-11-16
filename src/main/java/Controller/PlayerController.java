f0abb3d3a4c69facc23fde94688b89b875c6a01f
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-11-17 01:10:21 +0330

    Edit PlayerController class & add all Functions.

--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -1,7 +1,29 @@
 package Controller;
 
-import Model.Player;
 
 public class PlayerController {
 
+    public void showPoints() {
+        //TODO
+    }
+
+    public void viewFavoriteGames() {
+        //TODO
+    }
+
+    public void viewPlatoMessages() {
+        //TODO
+    }
+
+    public void viewLastPlayed() {
+        //TODO
+    }
+
+    public void viewAdminSuggestions() {
+        //TODO
+    }
+
+    public void addFriend(String name) {
+        //TODO
+    }
 }
