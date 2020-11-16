d8355850b5bc53ce84ccefd356dc54453de7008f
aminlotfi <a.lotfi1201@gmail.com>
2020-11-16 21:02:14 +0330

    add Message class with their methods.

---
+++ src/main/java/Model/Message.java
@@ -0,0 +1,42 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Message {
+    private long massageID;
+    private Player player;
+    private String message;
+    // can not import LocalDateTime!!
+    private static ArrayList<Message> allMessages;
+
+    static {
+        allMessages = new ArrayList<Message>();
+    }
+
+    public Message(Player player, String message) {
+        this.player = player;
+        this.message = message;
+        //LocalDateTime
+        //massageID
+    }
+
+    public void addNewMessage(Message message) {
+        //TODO
+    }
+
+    public static ArrayList<Message> getAllMessages() {
+        return allMessages;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public long getMassageID() {
+        return massageID;
+    }
+}
