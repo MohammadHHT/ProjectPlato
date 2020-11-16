cbac69ffc5cfa70d2da3b6cecab3e2b830306719
aminlotfi <a.lotfi1201@gmail.com>
2020-11-16 20:37:54 +0330

    add Suggestion class.

---
+++ src/main/java/Model/Suggestion.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Suggestion {
+    private Player player;
+    private String gameName;
+
+    public Suggestion(Player player, String gameName) {
+        this.player = player;
+        this.gameName = gameName;
+    }
+}
