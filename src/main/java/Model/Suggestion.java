785396f6eabfb2ea8906b3b40c355457c9d32508
aminlotfi <a.lotfi1201@gmail.com>
2020-11-16 20:51:54 +0330

    add suggestionID field and allSuggestion List and setter & getter for them.

--- src/main/java/Model/Suggestion.java
+++ src/main/java/Model/Suggestion.java
@@ -1,12 +1,33 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Suggestion {
+    private long suggestionID;
     private Player player;
     private String gameName;
+    private static ArrayList<Suggestion> allSuggestion;
+
+    static {
+        allSuggestion = new ArrayList<Suggestion>();
+    }
 
     public Suggestion(Player player, String gameName) {
         this.player = player;
         this.gameName = gameName;
+        //suggestionID
+    }
+
+    public void addNewSuggestion(Suggestion suggestion) {
+        //TODO
+    }
+
+    public static ArrayList<Suggestion> getAllSuggestion() {
+        return allSuggestion;
+    }
+
+    public long getSuggestionID() {
+        return suggestionID;
     }
 
     public String getGameName() {
