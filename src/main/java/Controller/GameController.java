a016fa4f4b9caec12b8fbfe4eb4892fdd3cdbb8c
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-11-18 15:19:05 +0330

    Add GameController class & some functions.

---
+++ src/main/java/Controller/GameController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+public class GameController {
+
+    public void showScoreboard() {
+        //TODO
+    }
+
+    public void details() {
+        //TODO
+    }
+
+    public void showLog() {
+        //TODO
+    }
+
+    public int showWinsCount() {
+        //TODO
+    }
+}
