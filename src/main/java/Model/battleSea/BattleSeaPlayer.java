152d166be75bf7c4f5d034be0ba7364971ac7c48
aminlotfi <a.lotfi1201@gmail.com>
2020-11-19 19:54:02 +0330

    add isBombExist method for BattleSeaPlayer class.

--- src/main/java/Model/battleSea/BattleSeaPlayer.java
+++ src/main/java/Model/battleSea/BattleSeaPlayer.java
@@ -1,4 +1,15 @@
 package Model.battleSea;
 
+import java.util.ArrayList;
+
 public class BattleSeaPlayer {
+    private ArrayList<Bomb> bombsPlanted;
+
+    //TODO constructor
+
+    public boolean isBombExist (int x, int y) {
+        return true;
+    }
+
+
 }
