54ca78d58fc3bba7fa744816efa901fb50696566
aminlotfi <a.lotfi1201@gmail.com>
2020-11-19 20:07:27 +0330

    add isDestroyed method.

--- src/main/java/Model/battleSea/Bomb.java
+++ src/main/java/Model/battleSea/Bomb.java
@@ -3,16 +3,20 @@ package Model.battleSea;
 public class Bomb {
     private int x;
     private int y;
-    private boolean isDestroy;
+    private boolean isDestroyed;
 
-    public Bomb(int x, int y, boolean isDestroy) {
+    public Bomb(int x, int y, boolean isDestroyed) {
         this.x = x;
         this.y = y;
-        this.isDestroy = isDestroy;
+        this.isDestroyed = isDestroyed;
     }
 
     public Bomb plantBomb(int x, int y, BattleSeaPlayer planter) {
         return null;
+    }
+
+    public boolean isDestroyed (BattleSeaPlayer planter) {
+        return true;
     }
 
 
