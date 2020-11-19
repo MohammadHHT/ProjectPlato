d6bf760c58d5df3acdd5b560970dac019a7cc3c0
aminlotfi <a.lotfi1201@gmail.com>
2020-11-19 20:05:37 +0330

    add fields and constructor.

--- src/main/java/Model/battleSea/Bomb.java
+++ src/main/java/Model/battleSea/Bomb.java
@@ -1,4 +1,19 @@
 package Model.battleSea;
 
 public class Bomb {
+    private int x;
+    private int y;
+    private boolean isDestroy;
+
+    public Bomb(int x, int y, boolean isDestroy) {
+        this.x = x;
+        this.y = y;
+        this.isDestroy = isDestroy;
+    }
+
+    public Bomb plantBomb(int x, int y, BattleSeaPlayer planter) {
+        return null;
+    }
+
+
 }
