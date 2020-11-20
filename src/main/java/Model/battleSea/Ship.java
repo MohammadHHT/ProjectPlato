544022b0c555ae7a0bc94ec66723894a9efbba39
aminlotfi <a.lotfi1201@gmail.com>
2020-11-20 19:56:43 +0330

    add methods.

--- src/main/java/Model/battleSea/Ship.java
+++ src/main/java/Model/battleSea/Ship.java
@@ -10,4 +10,48 @@ public class Ship {
         this.length = length;
         this.isHorizontal = isHorizontal;
     }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHorizontal(boolean horizontal) {
+        isHorizontal = horizontal;
+    }
+
+    public boolean isHorizontal() {
+        return isHorizontal;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public boolean isWreck(BattleSeaPlayer player) {
+        return false;
+    }
+
+    public boolean canChangeDirection() {
+        return false;
+    }
+
+    public void changeDirection() {
+        //TODO
+    }
+
+    public boolean canMove(int x, int y) {
+        return false;
+    }
+
+    public void move(int x, int y) {
+        //TODO
+    }
 }
