8050ec47268a05f38dc372c14403fa4d462d5b22
Sadra <M.sadra.azimi@gmail.com>
2020-11-25 16:03:23 +0330

    remove pop from LoginMenu next method

--- src/main/java/View/LoginMenu.java
+++ src/main/java/View/LoginMenu.java
@@ -64,7 +64,6 @@ public class LoginMenu extends Menu {
     }
 
     public void next() {
-        pop();
         if (nextMenu == 1) {
             PrimaryMenu.getPrimaryMenu().run();
         } else {
