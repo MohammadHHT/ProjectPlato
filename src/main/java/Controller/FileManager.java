ba075f72c3a951886d2f082593275952413bcf9b
aminlotfi <a.lotfi1201@gmail.com>
2020-11-26 19:09:03 +0330

    fill loadUsersFromFile method.

--- src/main/java/Controller/FileManager.java
+++ src/main/java/Controller/FileManager.java
@@ -1,6 +1,17 @@
 package Controller;
 
-import Controller.Database.*;
+import static Controller.Database.*;
+import Model.Admin;
+import Model.Player;
+import Model.User;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class FileManager {
     public static void initialize(){
@@ -8,11 +19,31 @@ public class FileManager {
     }
 
     public static void loadUsersFromFile(String place) {
-        //TODO
+        Gson gson = new Gson();
+        File file = new File("Data\\Accounts\\"+place+".json");
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(file));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        ArrayList<User> array;
+        Type type;
+        if (place.equals("admin")) {
+            type = new TypeToken<ArrayList<Admin>>(){}.getType();
+        } else  {
+            type = new TypeToken<ArrayList<Player>>(){}.getType();
+        }
+        array = gson.fromJson(bufferedReader, type);
+        allUsers.addAll(array);
+
     }
 
     public static void saveUsersOnFile() {
         //TODO
     }
+
+
 
 }
