32a2df57a957aa01c837590dcab3c4f5be60e9f8
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-11-26 16:09:40 +0330

    Add RequestNotFoundException class.

---
+++ src/main/java/Exception/RequestNotFoundException.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class RequestNotFoundException extends Exception{
+    public RequestNotFoundException() {
+        super("There is no request from this user!");
+    }
+}
