3d3b7e3e86845fafd7fbb768e8d27eda3a0fa537
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-11-29 00:01:40 +0330

    Edit addEvent function in AdminController class.

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,12 +1,24 @@
 package controller;
 
+import exception.InvalidDateException;
+import exception.InvalidGameNameException;
+import model.Event;
+import model.Game;
+
 import java.util.Date;
 
 public class AdminController {
 
-    public String addEvent(String gameName, Date startDate, Date endDate, long eventScore) {
-        return ";)";
-        //TODO
+    public String addEvent(String gameName, Date startDate, Date endDate, long eventScore) throws InvalidGameNameException, InvalidDateException {
+        for (Game allGame : Database.getAllGames()) {
+            if (!allGame.getGameName().equals(gameName))
+                throw new InvalidGameNameException();
+        }
+        if (startDate.after(endDate) || endDate.before(startDate))
+            throw new InvalidDateException();
+            Event event = new Event(gameName, startDate, endDate, eventScore);
+            Database.addAllEvents(event);
+            return "The event was created successfully.";
     }
 
     public void viewEvent() {
