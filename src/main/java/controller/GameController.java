401311d6760989054b97bf1dc9fa127693c2ef5b
aminlotfi <a.lotfi1201@gmail.com>
2020-12-03 19:57:18 +0330

    fill showScoreboard method sorting procces not finished.

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,13 +1,31 @@
 package controller;
 
+import exception.GameNotFoundException;
 import exception.UsernameNotFoundException;
 import model.GameLog;
 import model.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameController {
 
-    public void showScoreboard(String gameName) {
-        //TODO
+    public void showScoreboard(String gameName) throws GameNotFoundException {
+        HashMap<String, Long> allPlayersScore = new HashMap<String, Long>();
+        if (gameName.equalsIgnoreCase("Dots And Boxes")) {
+            for (Map.Entry<String, Player> entry : Player.getPlayers().entrySet()) {
+                allPlayersScore.put(entry.getValue().getUsername(),
+                        GameLog.getAllGameLogs().get(Integer.parseInt(entry.getValue().getGameLogs().get(0))).getTakenScore());
+            }
+        } else if (gameName.equalsIgnoreCase("Sea Battle")) {
+            for (Map.Entry<String, Player> entry : Player.getPlayers().entrySet()) {
+                allPlayersScore.put(entry.getValue().getUsername(),
+                        GameLog.getAllGameLogs().get(Integer.parseInt(entry.getValue().getGameLogs().get(1))).getTakenScore());
+            }
+        } else {
+            throw new GameNotFoundException();
+        }
+        //TODO sorting
     }
 
     public void details(String gameName) {
