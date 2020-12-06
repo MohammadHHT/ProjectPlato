8f66949917ff1f5c76f3091c62ed0cfc7a340fae
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-12-06 19:25:36 +0330

    Edit FileManager.

--- src/main/java/controller/FileManager.java
+++ src/main/java/controller/FileManager.java
@@ -1,73 +1,69 @@
 package controller;
 
-import static controller.Database.*;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import model.Admin;
 import model.Player;
 import model.User;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.Scanner;
 
 public class FileManager {
-    public static void initialize(){
-        loadUsersFromFile("Admin");
-        loadUsersFromFile("Player");
+
+    public static void initialize() {
+        FileManager.loadAdmin();
+        FileManager.loadPlayer();
     }
 
-    public static void loadUsersFromFile(String place) {
+    public static void loadAdmin() {
         Gson gson = new Gson();
-        File file = new File("resource\\Accounts\\"+place+".json");
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader(file));
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        Scanner scanner = FileManager.openFileToRead("resources\\admin.json");
+        String data = scanner.nextLine();
+        Type foundListType = new TypeToken<ArrayList<Admin>>(){}.getType();
+        ArrayList<Admin> admins = gson.fromJson(data, foundListType);
+        scanner.close();
+        for (Admin admin : admins) {
+            User.addUser(admin);
         }
-
-        ArrayList<User> array;
-        Type type;
-        if (place.equals("admin")) {
-            type = new TypeToken<ArrayList<Admin>>(){}.getType();
-        } else  {
-            type = new TypeToken<ArrayList<Player>>(){}.getType();
-        }
-        array = gson.fromJson(bufferedReader, type);
-        allUsers.addAll(array);
-
     }
 
-    public static void saveUsersOnFile() {
-        ArrayList<Object> admin = new ArrayList<Object>();
-        ArrayList<Object> player = new ArrayList<Object>();
-        for (User user: allUsers) {
-            if (user instanceof Player) {
-                player.add(user);
-            } else if (user instanceof Admin) {
-                admin.add(user);
-            }
+    public static void loadPlayer() {
+        Gson gson = new Gson();
+        Scanner scanner = FileManager.openFileToRead("resources\\player.json");
+        String data = scanner.nextLine();
+        Type foundListType = new TypeToken<ArrayList<Player>>(){}.getType();
+        ArrayList<Player> players = gson.fromJson(data, foundListType);
+        scanner.close();
+        for (Player player : players) {
+            User.addUser(player);
+            Player.addPlayer(player);
         }
-        saveArrayOnFile(admin, "Accounts\\Admin");
-        saveArrayOnFile(player, "Accounts\\Player");
     }
 
-    private static void saveArrayOnFile(ArrayList<Object> array, String name) {
-        File file = new File("resource\\" + name + ".json");
-        file.getParentFile().mkdirs();
-        FileWriter fileWriter = null;
+    public static Formatter openFileToWrite(String address) {
+        Formatter formatter = null;
         try {
-            fileWriter = new FileWriter(file);
-            Gson gson = new Gson();
-            String json = gson.toJson(array);
-            fileWriter.write(json);
-            fileWriter.flush();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            new File(address.substring(0, address.lastIndexOf("\\"))).mkdir();
+            formatter = new Formatter(address);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+        return formatter;
+    }
+
+    public static Scanner openFileToRead(String address) {
+        Scanner scanner = null;
+        try {
+            File file = new File(address);
+            scanner = new Scanner(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return scanner;
     }
 }
