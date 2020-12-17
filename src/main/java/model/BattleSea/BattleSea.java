40cebbabf3fa5820bcf85ad09cf6c430d25496b5
aminlotfi <a.lotfi1201@gmail.com>
2020-12-17 20:43:38 +0330

    add ready method.

--- src/main/java/model/BattleSea/BattleSea.java
+++ src/main/java/model/BattleSea/BattleSea.java
@@ -1,20 +1,15 @@
 package model.BattleSea;
 
-import model.DotsAndBoxes.DotsAndBoxes;
 import model.Game;
 import model.Player;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class BattleSea extends Game {
     private static HashMap<Long, BattleSea> battleSeas = new HashMap<>();
 
-
-     BattleSeaPlayer host ;
-     BattleSeaPlayer guest ;
-     BattleSeaPlayer turn;
+    BattleSeaPlayer host ;
+    BattleSeaPlayer guest ;
+    BattleSeaPlayer turn;
 
     public BattleSea(Player host) {
         super();
@@ -25,6 +20,30 @@ public class BattleSea extends Game {
 
     public static HashMap<Long, BattleSea> getBattleSeas() {
         return battleSeas;
+    }
+
+    @Override
+    public void turn() {
+        if (turn.equals(host)) {
+            turn = guest;
+        } else {
+            turn = host;
+        }
+    }
+
+    @Override
+    public boolean join(Player guest) {
+        if (this.guest == null) {
+            this.guest = new BattleSeaPlayer(guest);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Player judge() {
+        return null;
     }
 
     private  void setupRandomBoard(BattleSeaPlayer battleSeaPlayer) {
@@ -54,36 +73,40 @@ public class BattleSea extends Game {
         }
     }
 
+    private void ready() {
+        turn.isReady = true;
+    }
+
     private void askForGuess(BattleSeaPlayer player, BattleSeaPlayer opponent, int x, int y) {
-        boolean flag = false;
-        // row == y  ,  col == x
+        if (turn.isReady){
+            boolean flag = false;
+            // row == y  ,  col == x
 
-        if (x < 0 || x > 9 || y < 0 || y > 9) {
-            System.out.println("Coordinates must be inside the grid.");
-        }
-
-        while (!flag) {
-            if (!opponent.playerGrid.alreadyGuessed(y, x)){
-                if (opponent.playerGrid.hasShip(y, x)) {
-                    opponent.playerGrid.markHit(y, x);
-                    System.out.println("Bombed successfully!!!");
-                    Ship oppShip = opponent.ships[convertLetterToInt(opponent.playerGrid.get(y, x).getShipName())];
-                    if (isShipDestroyCompletely(opponent, oppShip)) {
-                        changeDestroyedShipSign(opponent, oppShip);
-                    }
-
-
-
-                } else {
-                    opponent.playerGrid.markMiss(y, x);
-                    System.out.println("Bombed unsuccessfully!");
-                    flag = true;
-                }
-            } else {
-                System.out.println("Selected X,Y has been already boomed");
+            if (x < 0 || x > 9 || y < 0 || y > 9) {
+                System.out.println("Coordinates must be inside the grid.");
             }
-        }
 
+            while (!flag) {
+                if (!opponent.playerGrid.alreadyGuessed(y, x)){
+                    if (opponent.playerGrid.hasShip(y, x)) {
+                        opponent.playerGrid.markHit(y, x);
+                        System.out.println("Bombed successfully!!!");
+                        Ship oppShip = opponent.ships[convertLetterToInt(opponent.playerGrid.get(y, x).getShipName())];
+                        if (isShipDestroyCompletely(opponent, oppShip)) {
+                            changeDestroyedShipSign(opponent, oppShip);
+                        }
+                    } else {
+                        opponent.playerGrid.markMiss(y, x);
+                        System.out.println("Bombed unsuccessfully!");
+                        flag = true;
+                    }
+                } else {
+                    System.out.println("Selected X,Y has been already boomed");
+                }
+            }
+        } else {
+            System.out.println("You're not ready!");
+        }
     }
 
     private boolean isShipDestroyCompletely (BattleSeaPlayer battleSeaPlayer, Ship ship) {
@@ -195,51 +218,58 @@ public class BattleSea extends Game {
     }
 
     private void changeShipsLocation(BattleSeaPlayer battleSeaPlayer, String shipName , int newCol, int newRow) {
-        battleSeaPlayer.playerGrid.printShips();
-        System.out.println();
-        String input;
+        if (!turn.isReady) {
+            battleSeaPlayer.playerGrid.printShips();
+            System.out.println();
+            String input;
 
-        input = shipName.toUpperCase();
-        int selectedShip = convertLetterToInt(input);
+            input = shipName.toUpperCase();
+            int selectedShip = convertLetterToInt(input);
 
-        if (newCol >= 0 && newCol <= 9 && newRow >= 0 && newRow <= 9) {
-            if (!hasLocationError(newRow, newCol, battleSeaPlayer.ships[selectedShip].getDirection(),
-                    battleSeaPlayer, selectedShip)) {
-                battleSeaPlayer.ships[selectedShip].setLocation(newRow, newCol);
-                battleSeaPlayer.playerGrid.addShip(battleSeaPlayer.ships[selectedShip]);
-                battleSeaPlayer.playerGrid.printShips();
-                System.out.println("Ship " + input + " relocated " + "in (" + newRow + ", " + newCol + ") successfully!" );
-             } else {
-                System.out.println("Ship " + input + " can not relocate in (" + newRow + ", " + newCol + ").");
+            if (newCol >= 0 && newCol <= 9 && newRow >= 0 && newRow <= 9) {
+                if (!hasLocationError(newRow, newCol, battleSeaPlayer.ships[selectedShip].getDirection(),
+                        battleSeaPlayer, selectedShip)) {
+                    battleSeaPlayer.ships[selectedShip].setLocation(newRow, newCol);
+                    battleSeaPlayer.playerGrid.addShip(battleSeaPlayer.ships[selectedShip]);
+                    battleSeaPlayer.playerGrid.printShips();
+                    System.out.println("Ship " + input + " relocated " + "in (" + newRow + ", " + newCol + ") successfully!" );
+                } else {
+                    System.out.println("Ship " + input + " can not relocate in (" + newRow + ", " + newCol + ").");
+                }
             }
+        } else {
+            System.out.println("This command is for before starting the game.");
         }
     }
 
     private void changeShipsDirection(BattleSeaPlayer battleSeaPlayer, String shipName) {
-        battleSeaPlayer.playerGrid.printShips();
-        System.out.println();
-        String input;
-
-        input = shipName.toUpperCase();
-        int selectedShip = convertLetterToInt(input);
-        int row = battleSeaPlayer.ships[selectedShip].getRow();
-        int col = battleSeaPlayer.ships[selectedShip].getColumn();
-        int dir = battleSeaPlayer.ships[selectedShip].getDirection();
-        if (dir == 0) {
-            dir = 1;
-        } else {
-            dir = 0;
-        }
-        if (!hasLocationError(row, col, dir, battleSeaPlayer, selectedShip)) {
-            battleSeaPlayer.ships[selectedShip].setLocation(row, col);
-            battleSeaPlayer.ships[selectedShip].setDirection(dir);
-            battleSeaPlayer.playerGrid.addShip(battleSeaPlayer.ships[selectedShip]);
+        if (!turn.isReady) {
             battleSeaPlayer.playerGrid.printShips();
-            System.out.println("Ship " + input + " rotated successfully!" );
-        } else {
-            System.out.println("Ship " + input + " can not rotated.");
-        }
+            System.out.println();
+            String input;
 
+            input = shipName.toUpperCase();
+            int selectedShip = convertLetterToInt(input);
+            int row = battleSeaPlayer.ships[selectedShip].getRow();
+            int col = battleSeaPlayer.ships[selectedShip].getColumn();
+            int dir = battleSeaPlayer.ships[selectedShip].getDirection();
+            if (dir == 0) {
+                dir = 1;
+            } else {
+                dir = 0;
+            }
+            if (!hasLocationError(row, col, dir, battleSeaPlayer, selectedShip)) {
+                battleSeaPlayer.ships[selectedShip].setLocation(row, col);
+                battleSeaPlayer.ships[selectedShip].setDirection(dir);
+                battleSeaPlayer.playerGrid.addShip(battleSeaPlayer.ships[selectedShip]);
+                battleSeaPlayer.playerGrid.printShips();
+                System.out.println("Ship " + input + " rotated successfully!" );
+            } else {
+                System.out.println("Ship " + input + " can not rotated.");
+            }
+        } else {
+            System.out.println("This command is for before starting the game.");
+        }
     }
 
     private int convertLetterToInt(String letter) {
@@ -265,29 +295,5 @@ public class BattleSea extends Game {
                 break;
         }
         return toReturn;
-    }
-
-    @Override
-    public void turn() {
-        if (turn.equals(host)) {
-            turn = guest;
-        } else {
-            turn = host;
-        }
-    }
-
-    @Override
-    public boolean join(Player guest) {
-        if (this.guest == null) {
-            this.guest = new BattleSeaPlayer(guest);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public Player judge() {
-        return null;
     }
 }
