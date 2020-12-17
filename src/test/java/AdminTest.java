10f14e901876ce6b8885a187214c68622ad30206
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-12-17 21:18:29 +0330

    Add AdminTest

---
+++ src/test/java/AdminTest.java
@@ -0,0 +1,18 @@
+import model.Admin;
+import model.User;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+
+public class AdminTest {
+    public void addNewAdmin() {
+        Admin admin = new Admin("firstName", "lastName",
+                "userName", "password",
+                "example@exp.exp", "09000000000");
+        ArrayList<User> admins = new ArrayList<>();
+        Admin.addAdmins(admins);
+
+        Assert.assertEquals(Admin.getAdmins().get("userName").getUsername(), "userName");
+        Assert.assertEquals(Admin.getAdmins().size(), 1);
+    }
+}
