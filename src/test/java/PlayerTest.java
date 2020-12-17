9a3b60ae7c862fc947259adbbec4a864d59f794b
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-12-17 21:06:45 +0330

    Add PlayerTest

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,2 +1,21 @@
+import model.Player;
+import model.User;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class PlayerTest {
+
+    @Test
+    public void addNewPlayer() {
+        Player player = new Player("firstName", "lastName",
+                "userName", "password",
+                "example@exp.exp", "09000000000");
+        ArrayList<User> players = new ArrayList<>();
+        Player.addPlayers(players);
+
+        Assert.assertEquals(Player.getPlayers().get("userName").getUsername(), "userName");
+        Assert.assertEquals(Player.getPlayers().size(), 1);
+    }
 }
