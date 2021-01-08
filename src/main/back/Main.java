7052b46253f9c2ca8b8757ce1cc668b0a61f8407
mahdi hadi <mahdihadiam@gmail.com>
2021-01-08 13:05:16 +0330

    server updated

--- src/main/back/Main.java
+++ src/main/back/Main.java
@@ -3,7 +3,7 @@ import controller.Server;
 
 public class Main {
     public static void main(String[] args) {
-        new Admin("bottt", "bottt", "bottt", "bottt", "bot@gamil.com", "+989111234567");
+        new Admin("bottt", "bottt", "bottt", "bottt", "bot@gamil.com", "989111234567");
         new Server().start();
     }
 }
