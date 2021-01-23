a6fc8f19bd87b235b2690993f202dfa0e00843ea
aminlotfi <a.lotfi1201@gmail.com>
2021-01-23 16:08:35 +0330

    Added Dots & Boxes tutorial.

--- src/main/front/js/gameMenu.js
+++ src/main/front/js/gameMenu.js
@@ -19,11 +19,19 @@ function showSeaBattleLeaderboardDiv() {
 }
 
 function showDotsAndBoxesHistoryDiv() {
+    document.getElementById("DB-HowToPlayDiv").style.display = "none";
     document.getElementById("DB-leaderboardDiv").style.display = "none";
     document.getElementById("DB-historyDiv").style.display = "flex";
 }
 
 function showDotsAndBoxesLeaderboardDiv() {
+    document.getElementById("DB-HowToPlayDiv").style.display = "none";
     document.getElementById("DB-historyDiv").style.display = "none";
     document.getElementById("DB-leaderboardDiv").style.display = "flex";
+}
+
+function showDotsAndBoxesHowToPlay() {
+    document.getElementById("DB-historyDiv").style.display = "none";
+    document.getElementById("DB-leaderboardDiv").style.display = "none";
+    document.getElementById("DB-HowToPlayDiv").style.display = "flex";
 }
