0987d52c3293e75db75a267b912c15491d8020ba
aminlotfi <a.lotfi1201@gmail.com>
2021-02-07 13:15:54 +0330

    connecting suggestion menu to back.

--- src/main/front/js/suggestion.js
+++ src/main/front/js/suggestion.js
@@ -1,3 +1,22 @@
-function chatWithPlayer() {
+function suggestion(username, token) {
+    this.username = username;
+    this.token = token;
+    this.init();
+}
 
+suggestion.prototype.init = function () {
+    const self = this;
+
+    document.querySelector().addEventListener('click', () => {
+        const connection = new WebSocket('ws://127.0.0.1:4444');
+
+        connection.onopen = function () {
+            connection.send('user allUsers' + self.username + ' ' + self.token);
+        };
+
+        connection.onmessage = function (e) {
+
+            connection.close();
+        };
+    })
 }
